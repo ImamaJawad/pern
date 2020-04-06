c0ac2c39a4d2c666748a30b8c3c586ba5b1d81db
Imama Jawad <ijawad.bese16seecs@seecs.edu.pk>
2020-04-06 17:10:53 +0500

    add client
    cside code

---
+++ client/build/precache-manifest.74178b592c43270d85a387f8d7daebed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "801ca35f01475f6438f24e3f4fe68f1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "896fd5c432f00b913bcb",
+    "url": "/static/css/main.bf1ba70f.chunk.css"
+  },
+  {
+    "revision": "86a868f8af9d21af6f6e",
+    "url": "/static/js/2.ba27f52c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba27f52c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "896fd5c432f00b913bcb",
+    "url": "/static/js/main.791222b7.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
